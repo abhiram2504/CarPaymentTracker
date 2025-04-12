775eb0be57d0ffe3a5767ab58cf43185e8a0414b
abhiram2504 <abhiram.rustagi@gmail.com>
2025-04-12 13:05:51 -0400

    re init

---
+++ src/components/CarPoolScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const CarPoolScreen = () => {
+    return (
+        <>
+            <View>
+            <Text>CarPoolScreen</Text>
+            </View>
+        </>
+    );
+}
+
+export default CarPoolScreen;
